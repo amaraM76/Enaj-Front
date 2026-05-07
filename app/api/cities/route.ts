b7fd06323ac43e9d889dc6969b5360052a3f9bd5
amaraM76 <136095008+amaraM76@users.noreply.github.com>
2026-05-07 14:06:02 -0400

    testing if fetch for cities work part3

--- app/api/cities/route.ts
+++ app/api/cities/route.ts
@@ -4,9 +4,7 @@ export async function GET(request: NextRequest) {
   const stateCode = request.nextUrl.searchParams.get('state')
   if (!stateCode) return NextResponse.json({ cities: [] })
 
-  const username = process.env.GEONAMES_USERNAME
-  const url = `https://secure.geonames.org/searchJSON?country=US&adminCode1=${stateCode}&featureClass=P&maxRows=1000&orderby=population&username=${username}`
-
+  const url = `http://api.geonames.org/searchJSON?country=US&adminCode1=${stateCode}&featureClass=P&maxRows=1000&orderby=population&username=enajhealth`
   try {
     const res = await fetch(url, { cache: 'no-store' })
     const data = await res.json()
