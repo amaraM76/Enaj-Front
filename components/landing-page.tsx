133a16c6f68ded05a06e837a582d46b27c3f0542
amaraM76 <136095008+amaraM76@users.noreply.github.com>
2026-06-04 11:52:10 -0400

    add back right logo

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -278,7 +278,7 @@ export function LandingPage() {
         </div>
         
         {/* Flying Logo with Gold Bag */}
-        <div className="pointer-events-none absolute left-[8%] top-1/2 -translate-y-1/2 z-[1] hidden md:block" aria-hidden="true">
+        <div className="pointer-events-none absolute right-[8%] top-1/2 -translate-y-1/2 z-[1] hidden md:block" aria-hidden="true">
           <FlyingLogoWithBag />
         </div>
         <div className="relative z-10 mx-auto max-w-3xl text-center"></div>
